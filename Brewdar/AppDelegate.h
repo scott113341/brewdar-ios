99c184220ab2b4701d776fa5e45533e0f3f62312
Scott Hardy <scott.the.hardy@gmail.com>
2014-10-08 16:51:23 -0700

    Initial Commit

---
+++ Brewdar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Brewdar
+//
+//  Created by Scott Hardy on 10/8/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Brewdar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Brewdar
+//
+//  Created by Scott Hardy on 10/8/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
