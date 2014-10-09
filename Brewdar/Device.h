8a7420566e377538d41987c8beb7a560b04886df
Scott Hardy <scott.the.hardy@gmail.com>
2014-10-08 18:33:46 -0700

    made realm device model

---
+++ Brewdar/Device.h
@@ -0,0 +1,22 @@
+//
+//  Device.h
+//  Brewdar
+//
+//  Created by Scott Hardy on 10/8/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface Device : RLMObject
+
+@property NSString *email;
+@property NSString *deviceId;
+@property NSString *name;
+@property NSString *authenticationToken;
+
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<Device>
+RLM_ARRAY_TYPE(Device)
---
+++ Brewdar/Device.m
@@ -0,0 +1,27 @@
+//
+//  Device.m
+//  Brewdar
+//
+//  Created by Scott Hardy on 10/8/14.
+//  Copyright (c) 2014 PWNZONE. All rights reserved.
+//
+
+#import "Device.h"
+
+@implementation Device
+
+// Specify default values for properties
+
+//+ (NSDictionary *)defaultPropertyValues
+//{
+//    return @{};
+//}
+
+// Specify properties to ignore (Realm won't persist these)
+
+//+ (NSArray *)ignoredProperties
+//{
+//    return @[];
+//}
+
+@end
